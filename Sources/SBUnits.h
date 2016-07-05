90ea6429fd661d9a9ccb098351406b53298e0027
Ed Gamble <ebg@opuslogica.com>
2016-07-05 08:00:31 -0700

    Avoid 'implicit' import of 'Foundation'

--- Sources/SBUnits.h
+++ Sources/SBUnits.h
@@ -6,13 +6,7 @@
 //  Copyright © 2015 Opus Logica Inc. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for SBUnits.
-FOUNDATION_EXPORT double SBUnitsVersionNumber;
-
-//! Project version string for SBUnits.
-FOUNDATION_EXPORT const unsigned char SBUnitsVersionString[];
+// Avoid import of Foundation
 
 // In this header, you should import all the public headers of your framework using statements like #import <SBUnits/PublicHeader.h>
 
