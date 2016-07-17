aaa6b3e2d4b14375518775ab9b8945fb1423a01f
Ed Gamble <ebg@opuslogica.com>
2016-07-17 16:39:17 -0700

    Remove dependency on 'Foundation'

--- Sources/SBUnits.h
+++ Sources/SBUnits.h
@@ -5,9 +5,3 @@
 //  Created by Ed Gamble on 11/20/15.
 //  Copyright © 2015 Opus Logica Inc. All rights reserved.
 //
-
-// Avoid import of Foundation
-
-// In this header, you should import all the public headers of your framework using statements like #import <SBUnits/PublicHeader.h>
-
-
